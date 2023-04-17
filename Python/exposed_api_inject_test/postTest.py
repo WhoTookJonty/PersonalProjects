aa4c243fa5298a0fff1a2aba87624d7c4218f68f
Jonty <46462272+WhoTookJonty@users.noreply.github.com>
2023-04-17 00:28:51 -0400

    python api injection test

---
+++ Python/exposed_api_inject_test/postTest.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+r = requests.post("http://localhost:5000/api/users", 
+json={
+"firstName": "injected",
+"lastName" : "injected",
+"email" : "injected@email.com",
+"password" : "injected",
+})
+
+
+print(f"Status Code: {r.status_code}, Response: {r.json()}")
